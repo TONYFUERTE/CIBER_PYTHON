31f117e9871d3a0b2c81fba6dcd9f9191d378ded
TONYFUERTE <juanantonio.majada@gmail.com>
2024-10-17 15:40:17 +0100

    commit funciones

---
+++ PRÁCTICAS/PRÁCTICA 1/funciones.py
@@ -0,0 +1,30 @@
+# def saludar():
+#     print('Hola')
+
+# saludar()
+
+# def saludar_persona(nombre):
+#     print('Hola ' + nombre)
+
+# saludar_persona('Pepe')
+
+# def multiplicar(num1,num2):
+#     return num1 * num2
+
+# resultado = multiplicar(5,10)
+# print(resultado)
+
+# def dividir(num1,num2):
+#     total= num1 / num2
+#     return total
+
+# resultado= dividir(200,5)
+# print(resultado)
+
+#FUNCIONES DINÁMICAS
+
+def chequear_3_cifras(numero):
+    return numero in range(100,1000)
+
+resultado = chequear_3_cifras(suma)
+print(resultado)
